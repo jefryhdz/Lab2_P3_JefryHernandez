2cb723bf1691dd6adba70759ed119906037ab3e9
jefryhdz <jefryhdz@gmail.com>
2017-10-20 14:59:46 -0600

    segundo ejercicio

--- Lab2_JefryHernandez.cpp
+++ Lab2_JefryHernandez.cpp
@@ -3,16 +3,20 @@
 using namespace std;
 int menu();
 int primos();
+int multiplos();
 
 int main(){
 	switch(menu()){
 	int a;
-		case 1:{a=primos();cout<<"La suma de todos los numeros primos es:"<<a<<endl;}
-	}
+	int b;
+		case 1:{a=primos();cout<<"La suma de todos los numeros primos e:"<<a<<endl;}
+		case 2:{b= multiplos();cout<<"La suma de todos los numeros multiplos es:"<<b<<endl;}
+			
+			
 
 
 	return 0;
-}
+}}
 int menu(){
 	int menu; cout<<"     Menú"<<endl; cout<<"1. Suma de primos"<<endl;
 	cout<<"2. Suma de los multiplos"<<endl;
@@ -37,6 +41,23 @@ int primos(){
 			suma+=i;
 	}
 	return suma;
-
-
 }
+int multiplos(){
+	int a=0;
+	int b=0;
+	int n=0;
+	int suma =0;
+	cout<<"Ingrese el numero valor de a"<<endl;
+	cin>>a;
+	cout<<"Ingrese el numero valor b"<<endl;
+	cin>>b;
+	cout<<"Ingrese el numero valor para n"<<endl;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		if(i%a==0||i%b==0){
+			suma+=i;
+		}			
+	}
+	return suma;
+}
+
